8d92d5690f296b8d6397f2897972180f22d88eb3
SunriseSchoolAWB <sunriseschoolaurangabad@gmail.com>
2022-03-31 20:36:30 +0530

    minor improvements

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-const courses = ['PG', 'NUR', 'LKG', 'UKG', '1', '2', '3', '4', '5'];
+const courses = ['PG', 'NR', 'LKG', 'UKG', '1', '2', '3', '4', '5'];
 const course_info = {
-    'PG': {'age': '2-3', 'color1': '#4C6D93', 'color2':'#C8DEE5'},
-    'NUR': {'age': '3-4', 'color1': '#846446', 'color2':'#E7D6C7'},
-    'UKG': {'age': '5-6', 'color1': '#4A8069', 'color2':'#CEE8E3'},
-    'LKG': {'age': '4-5', 'color1': '#7C447D', 'color2':'#EBD5EC'},
-    '1': {'age': '6-7', 'color1': '#1F446E', 'color2':'#B6D0ED'},
-    '2': {'age': '7-8', 'color1': '#79413E', 'color2':'#E9D2D0'},
-    '3': {'age': '8-9', 'color1': '#574C9A', 'color2':'#E0DBFF'},
-    '4': {'age': '9-10', 'color1': '#C24F21', 'color2':'#FECACA'},
-    '5': {'age': '10-11', 'color1': '#2C6C1E', 'color2':'#B9D6B4'}
+    'PG': {'name':'Playclass', 'age': '2-3', 'color1': '#4C6D93', 'color2':'#C8DEE5'},
+    'NR': {'name':'Nursery', 'age': '3-4', 'color1': '#846446', 'color2':'#E7D6C7'},
+    'UKG': {'name':'Upper Kindergarten', 'age': '5-6', 'color1': '#4A8069', 'color2':'#CEE8E3'},
+    'LKG': {'name':'Lower Kindergarten', 'age': '4-5', 'color1': '#7C447D', 'color2':'#EBD5EC'},
+    '1': {'name':'1', 'age': '6-7', 'color1': '#1F446E', 'color2':'#B6D0ED'},
+    '2': {'name':'2', 'age': '7-8', 'color1': '#79413E', 'color2':'#E9D2D0'},
+    '3': {'name':'3', 'age': '8-9', 'color1': '#574C9A', 'color2':'#E0DBFF'},
+    '4': {'name':'4', 'age': '9-10', 'color1': '#C24F21', 'color2':'#FECACA'},
+    '5': {'name':'5', 'age': '10-11', 'color1': '#2C6C1E', 'color2':'#B9D6B4'}
 };
 var month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
@@ -71,7 +71,7 @@ async function beginLoading(){
     course_upper_card.addEventListener('transitionend', function() {
         var current_course = document.getElementById("course-current-card");
         var current_course_name = current_course.getAttribute('name');
-        course_upper_card.setAttribute('style', 'height: 55%; width: 100%; background-size: 200% 100%; border-radius: 20px 20px 0px 0px; display: inline-flex; justify-content: space-around; align-items: center; font-family: section-body-font; transition: none; background-color: '+course_info[current_course_name]["color2"]+';');
+        course_upper_card.setAttribute('style', 'height: 60%; width: 100%; background-size: 200% 100%; border-radius: 20px 20px 0px 0px; display: inline-flex; justify-content: space-around; align-items: center; font-family: section-body-font; transition: none; background-color: '+course_info[current_course_name]["color2"]+';');
       });
 }
 
@@ -102,7 +102,7 @@ function selectCourse(dir){
         var next_course = courses[current_course_index+dir];
         current_course.setAttribute('name', next_course);
         current_course.innerHTML = '<p>'+next_course+'</p>';
-        document.getElementById("course-current-class").innerText = next_course;
+        document.getElementById("course-current-class").innerText = course_info[next_course]["name"];
         document.getElementById("course-current-age").innerText = course_info[next_course]["age"];
         document.getElementById("course-current-card").style.setProperty('border-color', course_info[next_course]["color1"]);
         document.getElementById("path902").style.setProperty('stroke', course_info[next_course]["color1"]);
